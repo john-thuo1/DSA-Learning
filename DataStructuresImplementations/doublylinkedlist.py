3ccb8b7a2e8f38ae12b3798a33b4fcea033a7920
john-thuo1 <108690517+john-thuo1@users.noreply.github.com>
2023-01-09 09:13:01 +0300

    modified

--- DataStructuresImplementations/doublylinkedlist.py
+++ DataStructuresImplementations/doublylinkedlist.py
@@ -57,7 +57,70 @@ class Doublylinkedlist:
             current.nref = newNode 
             # Change the previous Ref of New Node to point to the initially last node   
             newNode.pref = current
-        
+
+    def addAfter(self, xNode, data):
+        # Check if list is empty or not
+        if self.head is None:
+            return "Doubly Linked list is empty"
+        else:
+            current = self.head
+            # You will be able to exit the loop on 2 conditions :
+            # 1. If the current node is None or if current.data is equal to xNode value
+            while current !=None:
+                if current.data == xNode:
+                    break
+                current = current.nref
+            
+            if current is None:
+                return "Given Node is not Proesent in the Doubly Linked List"
+            else:
+                # Create the New Node
+                newNode = Node(data)
+                # New Node should store the reference of the next node
+                # which is currently stored at current.nref
+                newNode.nref = current.nref
+                # New Node previous reference should point to the current node
+                newNode.pref = current
+               
+               # Checks if we are inserting the new node after the last node 
+                if current.nref is not None:
+                    #The node that is after the New Node should 
+                    # store the reference to the new node in its pref
+                    current.nref.pref = newNode
+                    
+                    # Current Node next reference should store the new node reference
+                current.nref = newNode
+                
+                
+            
+    def addBefore(self, xNode, data):
+        # Check if list is empty or not
+        if self.head is None:
+            return "Doubly Linked list is empty"
+        else:
+            current = self.head
+            # You will be able to exit the loop on 2 conditions :
+            # 1. If the current node is None or if current.data is equal to xNode value
+            while current !=None:
+                if current.data == xNode:
+                    break
+                current = current.nref
+            
+            if current is None:
+                return "Given Node is not Proesent in the Doubly Linked List"            
+            else:
+                newNode = Node(data)
+                newNode.nref = current
+                
+                newNode.pref = current.pref
+                if current.pref is not None:
+                    current.pref.nref = newNode
+                else:
+                    self.head = newNode
+                    
+                current.pref = newNode
+                
+                       
 node = Doublylinkedlist()
 node.addStart(10)
 node.addEnd(20)
